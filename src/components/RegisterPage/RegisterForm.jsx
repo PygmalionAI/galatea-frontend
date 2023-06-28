44e1b46c0701418a609c6e88d2eba572ba329780
OtisAlejandro <128675915+OtisAlejandro@users.noreply.github.com>
2023-06-28 18:35:51 -0500

    Register Page

---
+++ src/components/RegisterPage/RegisterForm.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { styles } from '../../styles';
+import { TextInput, Button } from '../Shared';
+
+const RegisterForm = (props) => {
+  const [isLoading, setIsLoading] = useState('');
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    // Perform any desired logic or actions
+  };
+
+  return (
+    <form onSubmit={(evt) => props.onSubmit(evt)} className="flex flex-col gap-6">
+      <div className="flex flex-col gap-2">
+        <TextInput fieldName="username" placeholder="Username" type="text" required />
+        <TextInput fieldName="email" placeholder="Email" type="email" required />
+        <TextInput
+        fieldName="password"
+        placeholder="Password"
+        type="password"
+        required
+        />
+        <TextInput
+        fieldName="confirmpassword"
+        placeholder="Confirm Password"
+        type="password"
+        required
+        />
+      </div>
+
+      <Button type="submit" disabled={props.isLoading} primary>
+      {props.isLoading ? "Logging in..." : "Log in"}
+      </Button>
+    </form>
+  )
+}
+
+export default RegisterForm
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import { Info, NavBar } from "./LandingPage";
 import { BetaInfo, BetaNavBar } from "./BetaPage";
 import { LoginNavBar, LoginForm } from "./LoginPage";
 import { TextInput, Button } from "./Shared";
-import { Header, InputBar } from './ChatPage'
-export { Info, NavBar, BetaInfo, BetaNavBar, LoginNavBar, LoginForm, TextInput, Button, Header, InputBar };
+import { Header, InputBar } from './ChatPage';
+import { RegisterNavBar, RegisterForm } from './RegisterPage';
+export { Info, NavBar, BetaInfo, BetaNavBar, LoginNavBar, LoginForm, TextInput, Button, Header, InputBar, RegisterNavBar, RegisterForm };
