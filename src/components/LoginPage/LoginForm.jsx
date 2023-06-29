47c208298430984f830a174268cd10760032f216
peeepy <111094949+peeepy@users.noreply.github.com>
2023-06-29 01:23:16 +0100

    Feat: Add password hide/show toggle to register & login forms

--- src/components/LoginPage/LoginForm.jsx
+++ src/components/LoginPage/LoginForm.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { styles } from '../../styles';
 import { TextInput, Button } from '../Shared';
+import { Eye, EyeOff } from 'lucide-react'
 
 const LoginForm = (props) => {
   const [isLoading, setIsLoading] = useState('');
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
+  const [isConfirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
@@ -14,12 +17,22 @@ const LoginForm = (props) => {
     <form onSubmit={(evt) => props.onSubmit(evt)} className="flex flex-col gap-6">
       <div className="flex flex-col gap-2">
         <TextInput fieldName="email" placeholder="Email" type="email" required />
+        <div className="relative">
         <TextInput
-        fieldName="password"
-        placeholder="Password"
-        type="password"
+          fieldName="password"
+          placeholder="Password"
+          type={isPasswordVisible ? "text" : "password"}
         required
         />
+        <button
+        onClick={(e) => {
+        e.preventDefault();
+        setPasswordVisible(prevState => !prevState)}}
+        className="absolute right-2 top-1/2 transform -translate-y-1/2"
+      >
+          { isPasswordVisible ? <Eye /> : <EyeOff /> }
+      </button>
+      </div>
       </div>
 
       <Button type="submit" disabled={props.isLoading} primary>
--- src/components/RegisterPage/RegisterForm.jsx
+++ src/components/RegisterPage/RegisterForm.jsx
@@ -1,37 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { styles } from '../../styles';
+import { Eye, EyeOff } from 'lucide-react'
 import { TextInput, Button } from '../Shared';
 
 const RegisterForm = (props) => {
   const [isLoading, setIsLoading] = useState('');
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
+  const [isConfirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     // Perform any desired logic or actions
   };
 
+  const submitButtonRef = useRef();
+
   return (
     <form onSubmit={(evt) => props.onSubmit(evt)} className="flex flex-col gap-6">
       <div className="flex flex-col gap-2">
         <TextInput fieldName="username" placeholder="Username" type="text" required />
         <TextInput fieldName="email" placeholder="Email" type="email" required />
+      <div className="relative">
         <TextInput
-        fieldName="password"
-        placeholder="Password"
-        type="password"
-        required
+          fieldName="password"
+          placeholder="Password"
+          type={isPasswordVisible ? "text" : "password"}
+          required
         />
-        <TextInput
-        fieldName="confirmpassword"
-        placeholder="Confirm Password"
-        type="password"
-        required
-        />
+      <button
+        onClick={(e) => {
+        e.preventDefault();
+        setPasswordVisible(prevState => !prevState)}}
+        className="absolute right-2 top-1/2 transform -translate-y-1/2"
+      >
+          { isPasswordVisible ? <Eye /> : <EyeOff /> }
+      </button>
       </div>
 
-      <Button type="submit" disabled={props.isLoading} primary>
-      {props.isLoading ? "Creating Your Account..." : "Sign Up"}
+      <div className="relative">
+        <TextInput
+          fieldName="confirmpassword"
+          placeholder="Confirm Password"
+          type={isConfirmPasswordVisible ? "text" : "password"}
+          required
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              submitButtonRef.current.click();
+            }
+          }}
+        />
+       <button
+        onClick={(e) => {
+        e.preventDefault();
+        setConfirmPasswordVisible(prevState => !prevState);
+        }}
+        className="absolute right-2 top-1/2 transform -translate-y-1/2"
+        >
+        { isConfirmPasswordVisible ? <Eye /> : <EyeOff /> }
+        </button>
+      </div>
+      <Button type="submit" disabled={props.isLoading} primary ref={submitButtonRef}>
+        {props.isLoading ? "Creating Your Account..." : "Sign Up"}
       </Button>
+      </div>
     </form>
   )
 }
