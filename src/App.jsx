9d29172c75c51c99f228392ca837e6621d3949c0
peeepy <111094949+peeepy@users.noreply.github.com>
2023-06-30 13:17:16 +0100

    Add account page & routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Beta, Landing, Login, Chat, Register } from './pages';
+import { Beta, Landing, Login, Chat, Register, Account } from './pages';
 const App = () => {
   return (
     <Router>
@@ -10,6 +10,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/account" element={<Account/>}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/Account.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { NavBar } from '../components'
+
+const Account = () => {
+  return (
+    <NavBar/>
+  )
+}
+
+export default Account
