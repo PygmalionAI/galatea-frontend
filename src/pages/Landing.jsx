9974aa4e49f5b0ae0f78731a8c6ba933d3e0003f
Ahmed K. A <ahmadkabdullah@pm.me>
2023-07-20 17:52:27 +0300

    Move floating stars into separate component and use in pages

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,36 +1,16 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { LandingNavBar, Info } from '../components' 
+import { Info, LandingNavBar } from "../components";
+import Stars from "../components/Shared/Stars";
 
 const Landing = () => {
-  useEffect(() => {
-    const numStars = 10; // Number of shooting stars to generate
-
-    // Generate shooting stars
-    for (let i = 0; i < numStars; i++) {
-      const star = document.createElement('div');
-      star.classList.add('star');
-      star.style.animationDuration = `${Math.random() * 3 + 1}s`; // Random animation duration between 1 and 4 seconds
-      star.style.left = `${Math.random() * 100}%`; // Random horizontal position
-      star.style.top = `${Math.random() * 100}%`; // Random vertical position
-
-      document.querySelector('.star-container').appendChild(star);
-    }
-  }, []);
-
   return (
-    <>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-primary bg-cover bg-no-repeat bg-center relative">
-          <div className="absolute w-full h-full">
-            <div className="absolute inset-0 flex justify-center items-center star-container"></div>
-          </div>
-          <LandingNavBar />
-          <Info />
-        </div>
-        <div className="relative z-0"></div>
+    <div className="relative z-0 bg-primary">
+      <div className="relative bg-primary bg-cover bg-center bg-no-repeat">
+        <Stars />
+        <LandingNavBar />
+        <Info />
       </div>
-    </>
+      <div className="relative z-0"></div>
+    </div>
   );
 };
 
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,30 +1,11 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { NotFoundNavBar, NotFound } from '../components' 
+import { NotFound, NotFoundNavBar } from "../components";
 
 const NotFoundPage = () => {
-  useEffect(() => {
-    const numStars = 10; // Number of shooting stars to generate
-
-    // Generate shooting stars
-    for (let i = 0; i < numStars; i++) {
-      const star = document.createElement('div');
-      star.classList.add('star');
-      star.style.animationDuration = `${Math.random() * 3 + 1}s`; // Random animation duration between 1 and 4 seconds
-      star.style.left = `${Math.random() * 100}%`; // Random horizontal position
-      star.style.top = `${Math.random() * 100}%`; // Random vertical position
-
-      document.querySelector('.star-container').appendChild(star);
-    }
-  }, []);
-
   return (
     <>
       <div className="relative z-0 bg-primary">
-        <div className="bg-primary bg-cover bg-no-repeat bg-center relative">
-          <div className="absolute w-full h-full">
-            <div className="absolute inset-0 flex justify-center items-center star-container"></div>
-          </div>
+        <div className="relative bg-primary bg-cover bg-center bg-no-repeat">
+          <Stars />
           <NotFoundNavBar />
           <NotFound />
         </div>
