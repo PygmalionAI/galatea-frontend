b6e885521e5cd5ddf7ca53a59218de5051c79a8b
Ahmed K. A <ahmadkabdullah@pm.me>
2023-07-23 00:19:26 +0300

    Fix styling in contactUs page to match login

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -24,20 +24,22 @@ const ContactUs = () => {
         <SimpleNavBar />
       </div>
 
-      <div className="border-ga-gray-darker/50 z-100 bg-ga-black-lightest relative mx-auto max-w-lg rounded-3xl border p-8 shadow-md">
-        <h1 className="mb-4 text-3xl font-bold">Contact Us</h1>
-        <p className="mb-4">
-          We're excited to hear from you! If you have any questions,
-          suggestions, or just want to say hi, feel free to drop us a message
-          using the form below.
-        </p>
+      <div className="z-100 relative mx-auto flex max-w-lg flex-col gap-6 rounded-3xl p-8">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-3xl font-bold">Contact Us</h1>
+          <p className="text-ga-white-darker">
+            We're excited to hear from you! If you have any questions,
+            suggestions, or just want to say hi, feel free to drop us a message
+            using the form below.
+          </p>
+        </div>
 
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="75"
           height="75"
           viewBox="0 0 32 32"
-          className="text-ga-purple-default/50 absolute right-5 top-5 text-purple-400"
+          className="text-purple-400 absolute right-5 top-5 text-ga-purple-default"
         >
           <g fill="none" fill-rule="evenodd">
             <path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z" />
@@ -71,14 +73,17 @@ const ContactUs = () => {
             required
             setInputFn={(val) => setForm({ ...form, message: val })}
           />
+        </form>
+
+        <div className="flex flex-col gap-2">
           <Button type="submit" text="Send Message" className="w-full">
             Send
           </Button>
 
-          <p className="text-ga-gray-default text-center text-sm">
+          <p className="text-center text-sm text-ga-gray-lightest">
             We'll get back to you as soon as we can!
           </p>
-        </form>
+        </div>
       </div>
 
       <Footer />
