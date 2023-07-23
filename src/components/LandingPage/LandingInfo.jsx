0ae22dfaf1ea1c625545e992f0894d799997ee91
Ahmed K. A <ahmadkabdullah@pm.me>
2023-07-23 08:05:58 +0300

    Fix landing page's styling for content addition

--- src/components/LandingPage/LandingInfo.jsx
+++ src/components/LandingPage/LandingInfo.jsx
@@ -1,10 +1,8 @@
 const Info = () => {
   return (
-    <section className="relative mx-auto h-screen w-full">
-      <div
-        className={`px-custom absolute inset-0 top-[25%] mx-auto flex max-w-7xl flex-row items-start gap-5`}
-      >
-        <div className="mt-5 flex flex-col items-center justify-center">
+    <main className="px-custom mx-auto flex min-h-screen w-full max-w-7xl flex-col gap-20 py-40">
+      <section className="flex flex-row gap-8">
+        <div className="flex flex-col items-center justify-center">
           <div className="h-5 w-5 rounded-full bg-ga-violet" />
           <div className="violet-gradient h-40 w-1 sm:h-80" />
         </div>
@@ -19,12 +17,12 @@ const Info = () => {
             language models
             <br className="hidden sm:block" /> for chat and role-play purposes.
             <br className="hidden sm:block" />{" "}
-            <br className="hidden sm:block" />
+            <br className="mb-4 hidden sm:block" />
             Coming soon!
           </p>
         </div>
-      </div>
-    </section>
+      </section>
+    </main>
   );
 };
 
