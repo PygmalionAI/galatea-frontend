4fde44363a5662ae42163304102d61c6c1cc72ab
Ahmed K. A <ahmadkabdullah@pm.me>
2023-07-25 00:12:31 +0300

    Add basic setup with tailwind and prettier with its plugins

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div>
+			<h1 className="mb-2 mt-2 text-3xl font-bold text-ga-purple-default">
+				HELLO
+			</h1>
+		</div>
+	);
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+
+      "ga-white": {
+        lightest: "#fff",
+        default: "#f3f3f3",
+        darker: "#cfd3d6",
+      },
+
+      // could be improved
+      "ga-gray": {
+        lightest: "#91919a",
+        lighter: "#78787b",
+        default: "#52525b",
+        darker: "#3f3f46",
+      },
+
+      "ga-black": {
+        lightest: "#222828",
+        lighter: "#0d1017",
+        default: "#050816",
+        darker: "#030b1c",
+      },
+
+      // accent color
+      "ga-purple": {
+        lightest: "#c090cb",
+        lighter: "#b556b4",
+        default: "#aa4bae",
+        darker: "#9840a0",
+        darkest: "#401c4c",
+      },
+
+      // for toasts
+      "ga-green": "#4aee70",
+      "ga-red": "#ff5d4e",
+
+      // used for gradients
+      "ga-pink": "#ec008c",
+      "ga-orange": "#fc6767",
+      "ga-violet": "#d021a1",
+      "ga-blue": "#2088df",
+    },
+
+    extend: {
+      fontFamily: {
+        sans: ["Poppins"],
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+    },
+  },
+  plugins: [],
+}
+
