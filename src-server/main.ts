67da2cb4b02d06a28b0e8502785185bb6e768764
Ahmed K. A <ahmadkabdullah@pm.me>
2023-07-25 19:21:44 +0300

    Fix usage process.env to use typed env object

--- src-server/main.ts
+++ src-server/main.ts
@@ -29,7 +29,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = env.PORT || 3000;
 let server: any;
 
 app.use(cors());
