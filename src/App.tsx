8769cc69562de87074fc8ec2a214cccb84b1aca9
Ahmed K. A <ahmadkabdullah@pm.me>
2023-07-25 23:26:26 +0300

    Migrate assets and scripts and delete uneeded files

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-export default function App() {
-	return (
-		<div>
-			<h1 className="mb-2 mt-2 text-3xl font-bold text-ga-purple-default">
-				HELLO
-			</h1>
-		</div>
-	);
-}
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Account,
+  Beta,
+  Characters,
+  Chat,
+  Landing,
+  Login,
+  NotFoundPage,
+  Register,
+} from "./pages";
+import ContactUs from "./pages/ContactUs";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfService from "./pages/TermsOfService";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/beta" element={<Beta />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/terms" element={<TermsOfService />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
